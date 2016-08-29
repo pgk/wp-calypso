7a38ebd827fb1949e8f3822570a328be03f054df
Marko Andrijasevic <markoandrijasevic@gmail.com>
2016-08-29 16:28:30 +0200

    Refactor Pressable store component

--- client/signup/steps/design-type-with-store/index.jsx
+++ client/signup/steps/design-type-with-store/index.jsx
@@ -14,7 +14,6 @@ import Card from 'components/card';
 import { abtest } from 'lib/abtest';
 import { localize } from 'i18n-calypso';
 import { recordTracksEvent } from 'state/analytics/actions';
-
 import PressableStoreStep from './pressable-store';
 import BluehostStoreStep from './bluehost-store';
 import SitegroundStoreStep from './siteground-store';
@@ -36,10 +35,6 @@ class DesignTypeWithStoreStep extends Component {
 			{ type: 'store', label: this.props.translate( 'An online store' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 307 229"><g fill="none" fillRule="evenodd"><path fill="#C4EF96" d="M14 121h82v65H14zM14 201h82v28H14zM210 121h82v65h-82zM210 201h82v28h-82zM112 121h82v65h-82zM112 201h82v28h-82z"/><path fill="#E8F0F5" d="M0 0h307v105H0z"/><path d="M112 13h80v80h-80z"/><path d="M142 79.667c0 3.666-3 6.666-6.667 6.666-3.666 0-6.633-3-6.633-6.666C128.7 76 131.667 73 135.333 73 139 73 142 76 142 79.667zM168.667 73c-3.667 0-6.634 3-6.634 6.667 0 3.666 2.967 6.666 6.634 6.666 3.666 0 6.666-3 6.666-6.666 0-3.667-3-6.667-6.666-6.667zm1.32-16.667c3.123 0 5.83-2.17 6.506-5.22L182 29.667h-46.667v-3.334a6.665 6.665 0 0 0-6.666-6.666H122v6.666h6.667V63a6.665 6.665 0 0 0 6.666 6.667h40A6.665 6.665 0 0 0 168.667 63h-33.334v-6.667h34.654z" fill="#D2DEE6"/></g></svg> },
 		];
 	}
-
-	onPressableStoreStepRef = ( pressableStoreStep ) => {
-		this.pressableStore = pressableStoreStep;
-	};
 
 	scrollUp() {
 		// Didn't use setInterval in order to fix delayed scroll
@@ -123,7 +118,7 @@ class DesignTypeWithStoreStep extends Component {
 			default:
 				return <PressableStoreStep
 							{ ... this.props }
-							ref={ this.onPressableStoreStepRef }
+							ref={ ( ref ) => this.pressableStore = ref }
 							onBackClick={ this.handleStoreBackClick }
 						/>;
 		}
